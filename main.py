82acf0e4f50a63b778247ea5732dc2f23eb39641
Frederik Roland Christiansen <f.roland.c@gmail.com>
2022-10-05 13:03:11 +0200

    Så er der ryddet op i det hele

--- main.py
+++ main.py
@@ -2,14 +2,48 @@
 import cv2 as cv
 import numpy as np
 
-#Returnere et grayscale image ud fra det som man har puttet ind i funktionen
+#Læser billedet
+img = cv.imread('Images/coins_evenlyLit.png')
+
 def makeGrayscale(img):
+    """
+    Returnerer et grayscale image ud fra det som man har puttet ind i funktionen
+    :param img:
+    :return:
+    """
     output = np.zeros((img.shape[0],img.shape[1]), dtype = np.uint8)
     output[:,:] = img[:,:,0]*0.114 + img[:,:,1]*0.587 + img[:,:,2]*0.299
     return output
 
-#Returnere intensiteten af en enkelt pixel
+
 def calculateIntensity(pixel):
+    """
+    Returnerer intensiteten af en enkelt pixel
+    :param pixel:
+    :return:
+    """
     bgrMean = pixel[0] / 3 + pixel[1] / 3 + pixel[2] / 3
     intensity = bgrMean / 255
     return (intensity)
+
+
+def makeImageBinary(img,threshold):
+    """
+    :param img:
+    :param threshold:
+    :return black and white output:
+    -------
+        this is what the return returns
+
+    Funktion der gør pixels med en intensity værdi mindre end threshold sorte
+    og pixels med en intensity værdi større end threshold hvide
+    """
+    output = np.zeros(img.shape[0], img.shape[1], dtype= np.uint8)
+    for y, row in enumerate(img):
+        for x, pixel in enumerate(row):
+            if calculateIntensity(img[y, x]) < threshold:
+                output[y, x] = 255
+            else:
+                output[y, x] = 0
+    return output
+
