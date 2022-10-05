6d68e212912c590253e6ec9bae83fb14a8173808
Frederik Roland Christiansen <f.roland.c@gmail.com>
2022-10-05 10:10:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,15 +21,6 @@ def makeGrayscale(img):
     return output
 
 
-def areSame(A, B):
-    result = True
-    for i, row in enumerate(A):
-        for j, pixel in enumerate(A):
-            if (A[i][j] != B[i][j]):
-                result = False
-    return result
-
-
 def applyIntensity(img):
     #for y, row in enumerate(img):
     #    for x, pixel in enumerate(row):
@@ -39,17 +30,18 @@ def applyIntensity(img):
     #            intensityImg[y, x] = 0
 
     ret, thresh = cv.threshold(img, 127, 255, 0)
-    image, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    #image, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
     opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=1)
     closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel, iterations=10)
 
-    image, contours, hierarchy = cv.findContours(closing, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(closing, contours, -1, (0, 255, 0), 10)
+   # image, contours, hierarchy = cv.findContours(closing, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+   #cv.drawContours(closing, contours, -1, (0, 255, 0), 10)
+    print(areSame(img,makeGrayscale(img)))
     output = makeGrayscale(img)
     cv.imshow('input',img)
     cv.imshow('output', output)
-    print("NUMBER OF CUNTS: " + str(len(contours)))
+    #print("NUMBER OF CUNTS: " + str(len(contours)))
     cv.waitKey(0)
     cv.destroyAllWindows()
 
