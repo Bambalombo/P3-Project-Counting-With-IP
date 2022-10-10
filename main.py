9323dc80a9d153a0a635a5ff4fa2a92073ec7383
Frederik Roland Christiansen <f.roland.c@gmail.com>
2022-10-10 10:53:17 +0200

    A function was added using rgb color treshholding
    A function taking RGB min/max tresholdvalues, and tresholds the picture from these

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import cv2 as cv
 import numpy as np
 
 #Læser billedet
-img = cv.imread('Images/coins_evenlyLit.png')
+coinPicture = cv.imread('Images/coins_evenlyLit.png')
+fyrfadPicture = cv.imread('Images/fyrfadslys.jpg')
+colorTest = cv.imread('Images/RGB_test_billede.png')
 
 def makeGrayscale(img):
     """
@@ -27,14 +29,14 @@ def calculateIntensity(pixel):
     return (intensity)
 
 
-def makeImageBinary(img,threshold):
+def makeImageBinaryIntensityThreshold(img, threshold):
     """
     :param img:
     :param threshold:
     :return black and white output:
 
-    Funktion der gør pixels med en intensity værdi mindre end threshold sorte
-    og pixels med en intensity værdi større end threshold hvide
+    Funktion der gør pixels med en intensity værdi mindre end threshold hvide
+    og pixels med en intensity værdi større end threshold sorte
     Skal tage imod et bgr image
 
     Linus er den bedste 😎
@@ -49,11 +51,39 @@ def makeImageBinary(img,threshold):
                 output[y, x] = 0
     return output
 
-grayscaleImage = makeGrayscale(img)
-binaryImage = makeImageBinary(img, 0.5)
+def makeImageBinaryRGBThreshold(img, rThresholdMin, rThresholdMax, gThresholdMin, gThresholdMax, bThresholdMin, bThresholdMax):
+    """
 
-cv.imshow('original',img)
-cv.imshow('grayscale', grayscaleImage)
-cv.imshow('binary', binaryImage)
+    :param img:
+    :param rThresholdMin:
+    :param rThresholdMax:
+    :param gThresholdMin:
+    :param gThresholdMax:
+    :param bThresholdMin:
+    :param bThresholdMax:
+    :return black and white output image:
+
+    Laver et binært billede ud fra farve thresholds hvor man har en min og en maks threshold for hvert farve i rgb colorspace
+    """
+    output = np.zeros((img.shape[0],img.shape[1]), dtype=np.uint8)
+    for y, row in enumerate(img):
+        for x, pixel in enumerate(row):
+            if img[y, x, 0] >= bThresholdMin and img[y, x, 0] <= bThresholdMax and img[y, x, 1] >= gThresholdMin and img[y, x, 1] <= gThresholdMax and img[y, x, 2] >= rThresholdMin and img[y, x, 2] <= rThresholdMax:
+                output[y, x] = 255
+            else:
+                output[y, x] = 0
+    return output
+
+
+#grayscaleImage = makeGrayscale(picture)
+#binaryImage = makeImageBinaryIntensityThreshold(picture, 0.5)
+
+#binaryFromColor = makeImageBinaryRGBThreshold(fyrfadPicture,25,50,60,80,30,55)
+
+
+#cv.imshow('original',picture)
+#cv.imshow('grayscale', grayscaleImage)
+#cv.imshow('binary', binaryImage)
+#cv.imshow('binary from color', binaryFromColor)
 cv.waitKey(0)
 cv.destroyAllWindows()
