6f091684d72ca0aa4cacc4d0515ff6b5278af78b
Frederik Roland Christiansen <f.roland.c@gmail.com>
2022-10-14 11:03:27 +0200

    Grassfire + outline
    We've made a function taking the outline of objects in a binary image using errosion and thensubtracting the erroded image. Then weve made a grassfire algoritm detecting blobs, using them to count outlines in the outlined picture

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #Importer OpenCV og numpy
 import cv2 as cv
 import numpy as np
-
+from collections import deque
 #Læser billedet
-inputPicture = cv.imread('Images/coins_evenlyLit.png')
+inputPicture = cv.imread('Images/fyrfadslys.jpg')
 
 def makeGrayscale(img):
     """
@@ -64,7 +64,7 @@ def findAndDrawContours(img):
     return imageWithContours
 
 
-def edgeFromBinary(img):
+def edgeWithSobel(img):
     kernelRadius = 1
     sobelVerKernel = np.array([[-1,0,1],[-2,0,2],[-1,0,1]], dtype=np.uint8)
     sobelHorKernel = np.array([[1,2,1],[0,0,0],[-1,-2,-1]], dtype= np.uint8)
@@ -84,17 +84,94 @@ def edgeFromBinary(img):
 
     output = cv.add(verticalApply,horizontalApply)
     return output
+def addZeroPadding(img, sizey,sizex):
+    """
+
+    :param img:
+    :param sizey:
+    :param sizex:
+    :return:
+
+    rescales picture with zeropadding to size y,size x
+    """
+    paddedImage = np.zeros((sizey,sizex), dtype=np.uint8)
+    for y in range(img.shape[0]):
+        for x in range(img.shape[1]):
+            paddedImage[y+int((sizey-img.shape[0])/2),x+int((sizex-img.shape[1])/2)] = img[y,x]
+    return paddedImage
+
+def outlineFromBinary(img, kernelRadius):
+    """
+    :param img:
+    :param kernelRadius:
+    :return:
+
+    Funktion der laver et eroted billede og trækker det fra det originale billede,
+    for at få et billede med outlinen af objekter.
+
+    """
+    kernel = np.ones((kernelRadius*2+1,kernelRadius*2+1),dtype=np.uint8)*255
+    erodedImg = np.zeros((img.shape[0]-kernelRadius*2,img.shape[1]-kernelRadius*2), dtype=np.uint8)
+    #erodedImg = cv.erode(img, kernel)
+    for y in range(erodedImg.shape[0]):
+        for x in range(erodedImg.shape[1]):
+            slice = img[y:y+kernel.shape[0],x:x+kernel.shape[1]]
+            if np.allclose(kernel,slice):
+                erodedImg[y,x] = 255
+            else:
+                erodedImg[y,x] = 0
+
+    paddedImage = addZeroPadding(erodedImg,img.shape[0],img.shape[1])
+    output = cv.subtract(img,paddedImage)
+    return output
+
+def grassfire(img):
+    burningImage = addZeroPadding(img.copy(),img.shape[0]+2,img.shape[1]+2)
+
+    burnQueue = deque()
+    blobs = []
+    currentblob = []
+    imageNextPos = []
+    lastLoopingPixel = []
+    for y in range(burningImage.shape[0]-2):
+        for x in range(burningImage.shape[1]-2):
+            if not imageNextPos:
+                #print('i get next pixel')
+                imageNextPos = [y+1,x+1]
+                lastLoopingPixel = imageNextPos
+            else:
+                if lastLoopingPixel == imageNextPos:
+                    imageNextPos = [y + 1, x + 1]
+                    lastLoopingPixel = imageNextPos
+            if burningImage[imageNextPos[0],imageNextPos[1]] == 255 and imageNextPos not in blobs[:] and imageNextPos not in currentblob:
+                #print('i find white pixel')
+                currentblob.append([imageNextPos[0],imageNextPos[1]])
+                burningImage[imageNextPos[0],imageNextPos[1]] = 0
+                for yPixel in range(-1,2):
+                    for xPixel in range(-1,2):
+                        #print('checking for nearby pixels')
+                        if burningImage[imageNextPos[0]+yPixel,imageNextPos[1]+xPixel] == 255 and [imageNextPos[0]+yPixel,imageNextPos[1]+xPixel] not in currentblob and [imageNextPos[0]+yPixel,imageNextPos[1]+xPixel] not in burnQueue:
+                            #print('adding to burnqueue')
+                            burnQueue.append([imageNextPos[0]+yPixel,imageNextPos[1]+xPixel])
+                if len(burnQueue) != 0:
+                    #print('i get here ')
+                    imageNextPos = burnQueue.pop()
+                else:
+                    blobs.append(currentblob)
+                    print(currentblob)
+                    currentblob.clear()
+                    imageNextPos.clear()
+    print(str(len(blobs)))
 
 
-
-
-grayscaleImage = makeGrayscale(inputPicture)
 binaryImage = makeImageBinaryIntensityThreshold(inputPicture, 0.5)
 processedPicture = morphClose(binaryImage)
-edgedImage = edgeFromBinary(processedPicture)
+outlineImage = outlineFromBinary(processedPicture,3)
+grassfire(outlineImage)
 
-#cv.imshow('original',picture)
-cv.imshow('input', processedPicture)
-cv.imshow('edge', edgedImage)
+cv.imshow('original',inputPicture)
+cv.imshow('binary',binaryImage)
+cv.imshow('processed', processedPicture)
+cv.imshow('outline', outlineImage)
 cv.waitKey(0)
 cv.destroyAllWindows()
