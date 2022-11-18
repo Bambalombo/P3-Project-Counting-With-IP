91740e5928735a3af3a8613fdf149efd74557164
JensHejsel <ytnomicx@gmail.com>
2022-11-18 10:23:58 +0100

    The app can now take a picture and save it in a bitmap.

--- Android Studio Project folder/app/src/main/java/com/example/countingapplication/MainActivity.java
+++ Android Studio Project folder/app/src/main/java/com/example/countingapplication/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.countingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView myImage;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myImage = (ImageView) findViewById(R.id.myImage);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
+    }
+
+    private void takePicture() {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i,0);
+    }
+
+    protected void OnActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode == RESULT_OK){
+            Bitmap b = (Bitmap) data.getExtras().get("data");
+            myImage.setImageBitmap(b);
+        }
     }
 }
