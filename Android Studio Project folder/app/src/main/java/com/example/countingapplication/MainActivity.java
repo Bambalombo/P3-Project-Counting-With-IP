f56d6a8356ac6b6f06397cf87395d1028fe71857
JensHejsel <ytnomicx@gmail.com>
2022-11-24 11:51:23 +0100

    App

--- Android Studio Project folder/app/src/main/java/com/example/countingapplication/MainActivity.java
+++ Android Studio Project folder/app/src/main/java/com/example/countingapplication/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -17,18 +19,32 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView myImage;
-    private Button button;
+    private Button takePictureButton;
+    private Button markButton1;
+    private Button markButton2;
+    private Button markButton3;
+    private Button markButton4;
+    private Button[] buttonArray = new Button[4];
 
-    @SuppressLint("ClickableViewAccessibility")
+    @SuppressLint({"ClickableViewAccessibility", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         myImage = findViewById(R.id.myImage);
-        button = findViewById(R.id.button);
+        takePictureButton = findViewById(R.id.takePictureButton);
+        markButton1 = findViewById(R.id.markButton1);
+        markButton2 = findViewById(R.id.markButton2);
+        markButton3 = findViewById(R.id.markButton3);
+        markButton4 = findViewById(R.id.markButton4);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonArray[0] = markButton1;
+        buttonArray[1] = markButton2;
+        buttonArray[2] = markButton3;
+        buttonArray[3] = markButton4;
+
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent openCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -36,19 +52,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        myImage.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    int x = (int) event.getX();
-                    int y = (int) event.getY();
-                    Log.d("The X value is", String.valueOf(x));
-                    Log.d("The Y value is", String.valueOf(y));
+        for (Button button : buttonArray){
+            button.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent event) {
+                    float startX = 0, startY = 0;
+                    switch (event.getActionMasked()){
+                        case MotionEvent.ACTION_DOWN:
+                            startX = event.getX();
+                            startY = event.getY();
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            float endX, endY;
+                            float distanceX, distanceY;
+
+                            endX = event.getX();
+                            endY = event.getY();
+
+                            distanceX = endX-startX;
+                            distanceY = endY-startY;
+
+                            button.setX(button.getX()+distanceX);
+                            button.setY(button.getY()+distanceY);
+
+                            if (button.equals(markButton1)) {
+                                markButton2.setY(markButton1.getY());
+                                markButton3.setX(markButton1.getX());
+                            }
+                            else if (button.equals(markButton2)) {
+                                markButton1.setY(markButton2.getY());
+                                markButton4.setX(markButton2.getX());
+                            }
+                            else if (button.equals((markButton3))) {
+                                markButton1.setX(markButton3.getX());
+                                markButton4.setY(markButton3.getY());
+                            }
+                            else if (button.equals(markButton4)){
+                                markButton2.setX(markButton4.getX());
+                                markButton3.setY(markButton4.getY());
+                            }
+
+                            startX = endX;
+                            startY = endY;
+                            break;
+                        case MotionEvent.ACTION_UP:
+                            Rect mb1Rect = new Rect();
+                            markButton1.getLocalVisibleRect(mb1Rect);
+                            Log.d("Coords:", String.valueOf(mb1Rect));
+                            Log.d("left         :", String.valueOf(mb1Rect.left));
+                            Log.d("right        :", String.valueOf(mb1Rect.right));
+                            Log.d("top          :", String.valueOf(mb1Rect.top));
+                            Log.d("bottom       :", String.valueOf(mb1Rect.bottom));
+                            break;
+                    }
+                    return true;
                 }
-                return false;
-            }
-            
-        });
+            });
+        }
     }
 
     @Override
@@ -59,9 +119,4 @@ public class MainActivity extends AppCompatActivity {
             myImage.setImageBitmap(image);
         }
     }
-
-
-
 }
-
-
